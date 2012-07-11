93082055c4b0af3371b9a51e3c6552361014d7f2
Stephen McDonald <steve@jupo.org>
2012-07-11 17:38:43 +1000

    Fix path tweak for test project's manage.py

--- test_project/manage.py
+++ test_project/manage.py
@@ -2,7 +2,7 @@
 import sys
 import os
 
-sys.path.insert(0, os.path.join(os.path.dirname(__file__), '../..'))
+sys.path.insert(0, os.path.join(os.path.dirname(__file__), '..'))
 
 from django.core.management import execute_manager
 import imp
