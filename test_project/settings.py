65fa3cd11684f34905880d3994f29f6067f345db
Stephen McDonald <steve@jupo.org>
2013-12-29 13:26:54 +1100

    Update test env to Django 1.6

--- test_project/settings.py
+++ test_project/settings.py
@@ -6,6 +6,7 @@ PROJECT_ROOT = os.path.dirname(os.path.abspath(__file__))
 PROJECT_DIRNAME = PROJECT_ROOT.split(os.sep)[-1]
 ROOT_URLCONF = "%s.urls" % PROJECT_DIRNAME
 TEMPLATE_DIRS = (os.path.join(PROJECT_ROOT, "templates"),)
+TEST_RUNNER = "django.test.simple.DjangoTestSuiteRunner"
 SECRET_KEY = "hi mom"
 
 DATABASES = {
