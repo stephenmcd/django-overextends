c95c3e3c505ca46e62fc87690f36326c5579887b
Stefan Wehrmeyer <mail@stefanwehrmeyer.com>
2014-02-17 18:36:35 +0100

    Fix import path of add_to_builtins

--- overextends/models.py
+++ overextends/models.py
@@ -2,6 +2,7 @@
 # This app doesn't contain any models, but as its template tags need to
 # be added to built-ins at start-up time, this is a good place to do it.
 
-from django.template.loader import add_to_builtins
+from django.template.base import add_to_builtins
+
 
 add_to_builtins("overextends.templatetags.overextends_tags")
