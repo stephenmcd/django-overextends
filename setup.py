c6c680b32f9434af79a292b963ed1912e1657de0
Stephen McDonald <steve@jupo.org>
2015-07-23 09:51:41 +1000

    Update supported/testing Django versions.

--- setup.py
+++ setup.py
@@ -16,7 +16,7 @@ setup(
     packages = find_packages(),
     install_requires = [
         "sphinx-me >= 0.1.2",
-        "django >= 1.4",
+        "django >= 1.7, < 1.9",
     ],
     classifiers = [
         "Development Status :: 5 - Production/Stable",
